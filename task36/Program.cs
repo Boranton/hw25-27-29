e745cb220850ac5d0e28356db9edf7af84036747
«Anton» <mrs.ray.89@yandex.ru>
2022-12-01 23:41:33 +0300

    task36

---
+++ task36/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях(индексах).
+
+// [3, 7, 23, 12] -> 19
+
+// [-4, -6, 89, 6] -> 0
+
+
+int[] FillArray(int size)
+{
+    
+    int[] arr = new int[size];
+    for(int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(0,100);
+    }
+    return arr;
+}
+
+Console.Write("Введите длину массива: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int[] array = FillArray(num);
+Console.WriteLine("Ваш массив: " + string.Join(", ", array));
+
+int sum = 0;
+for (int i = 0; i < array.Length; i++)
+ {
+    if( i % 2 == 1)
+    {
+      sum = sum + array[i];
+     }     
+ }
+  Console.WriteLine("Сумма чисел, стоящих на нечетных индексах, равна: " + sum);
